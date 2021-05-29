0520a267958a48d0477f38af43356843e4dd4b35
TijmenHolt <tijmenholt@gmail.com>
2021-05-29 14:29:55 +0200

    Alles uit commentaar gehaald vanuit scaffold

--- estate2/controllers/controllers.py
+++ estate2/controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Estate2(http.Controller):
-#     @http.route('/estate2/estate2/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Estate2(http.Controller):
+     @http.route('/estate2/estate2/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
 
-#     @http.route('/estate2/estate2/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('estate2.listing', {
-#             'root': '/estate2/estate2',
-#             'objects': http.request.env['estate2.estate2'].search([]),
-#         })
+     @http.route('/estate2/estate2/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('estate2.listing', {
+             'root': '/estate2/estate2',
+             'objects': http.request.env['estate2.estate2'].search([]),
+         })
 
-#     @http.route('/estate2/estate2/objects/<model("estate2.estate2"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('estate2.object', {
-#             'object': obj
-#         })
+     @http.route('/estate2/estate2/objects/<model("estate2.estate2"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('estate2.object', {
+             'object': obj
+         })
