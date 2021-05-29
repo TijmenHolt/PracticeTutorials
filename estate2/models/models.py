b411744c2488699c030eae33cda6fbec968a2140
TijmenHolt <tijmenholt@gmail.com>
2021-05-29 14:57:41 +0200

    velden toegevoegd

--- estate2/models/models.py
+++ estate2/models/models.py
@@ -8,6 +8,21 @@ class estate2(models.Model):
      _description = 'estate2.estate2'
 
      name = fields.Char()
+     description = fields.Text()
+     postcode = fields.Char()
+     date_availability = fields.Date()
+     expected_price = fields.Float()
+     selling_price = fields.Floet()
+     bedrooms = fields.Integer()
+     living_area = fields.Integer()
+     facades = fields.Integer()
+     garage = fields.Boolean()
+     garden = fields.Boolean()
+     garden_area = fields.Integer()
+     garden_orientation = fields.Selection(
+         string='Garden Orientation',
+         selection=[('north', 'North'), ('east', 'East'), ('west', 'West'), ('south', 'South')],
+         help="Orientation is to indicate the garden orientation against the sun.")
      value = fields.Integer()
      value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
